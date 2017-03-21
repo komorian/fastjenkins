73f30931fcf66e756382b623d6b008478d1bf4e3
marcin <komorian@gmail.com>
2017-03-21 15:28:41 +0100

    Initial commit

---
+++ fastjenkins/fastjenkinsTests/fastjenkinsTests.swift
@@ -0,0 +1,36 @@
+//
+//  fastjenkinsTests.swift
+//  fastjenkinsTests
+//
+//  Created by Marcin Komorek on 21/03/2017.
+//  Copyright © 2017 MesMed. All rights reserved.
+//
+
+import XCTest
+@testable import fastjenkins
+
+class fastjenkinsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
